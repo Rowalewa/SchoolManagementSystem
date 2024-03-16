26989e8b177f267bde06fd6b41f34b003204f6d3
lesliewanjala06@gmail.com <lesliewanjala06@gmail.com>
2024-03-16 11:18:44 +0300

    Self

--- Users/models.py
+++ Users/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class Registration(models.Model):
-    first_name = models.CharField(max_length=10, default="")
-    last_name = models.CharField(max_length=15, default="")
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=15)
     email = models.EmailField()
     password = models.CharField(max_length=12)
     GENDER_CHOICES = (
